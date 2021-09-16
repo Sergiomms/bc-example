fa41e49739e0b669a7da6e12a4e086196d3b2a00
Sergiomms <mariano.srgio@gmail.com>
2021-09-16 20:03:58 -0300

    Projeto finalizado P.S. Kuppi

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import Home from '../Components/Home/Index';
+import ItemInfo from '../Components/Item_Infos/Index';
 
 import Route from './Route';
 
@@ -7,7 +9,9 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route exact path="/exemple" component={() => <div>exemple</div>} /> */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products/:id" component={ItemInfo} />
+
       </Switch>
     </BrowserRouter>
   );
